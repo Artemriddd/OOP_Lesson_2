3075a86d39763912b32adcd30b3f6586dab284cb
Artem <Artemrid@yandex.ru>
2022-07-01 17:20:23 +0300

    Добавил методы для считывания и записи полей из класса Check

---
+++ Ex_1/Ex_1/Check.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_1
+{
+    internal class Check
+    {
+        private int _Number;
+        private int _Balance;
+        public enum _Type { Wife, Husband };
+
+        public void PrintType(_Type type)
+        {
+            switch (type)
+            {
+                case _Type.Husband:
+                    Console.WriteLine("Счет мужа");
+                    break;
+                case _Type.Wife:
+                    Console.WriteLine("Счет жены");
+                    break;
+            }
+        }
+
+        public void setNumber(int n)
+        {
+            _Number = n;
+        }
+        public int getNumber()
+        {
+            return _Number;
+        }
+        public void setBalance(int n)
+        {
+            _Balance = n;
+        }
+        public int getBalance()
+        {
+            return _Balance;
+        }
+    }
+}
