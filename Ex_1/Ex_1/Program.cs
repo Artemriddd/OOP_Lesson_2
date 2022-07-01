ab3f21b30079c7874ca15e68202309ff65e9c5ae
Artem <Artemrid@yandex.ru>
2022-07-01 17:17:18 +0300

    Добавил методы на запись и считывание из класса check

--- Ex_1/Ex_1/Program.cs
+++ Ex_1/Ex_1/Program.cs
@@ -10,6 +10,25 @@ namespace Ex_1
     {
         static void Main(string[] args)
         {
+            Check checkHusband = new Check();
+            checkHusband.setNumber(1);
+            Console.Write("Номер счета: ");
+            Console.WriteLine(checkHusband.getNumber());
+            checkHusband.setBalance(1000);
+            Console.WriteLine("Баланс на счету: {0} рублей", checkHusband.getBalance());
+            checkHusband.PrintType(Check._Type.Husband);
+
+            Check checkWife = new Check();
+            checkWife.setNumber(2);
+            Console.Write("Номер счета: ");
+            Console.WriteLine(checkWife.getNumber());
+            checkWife.setBalance(2000);
+            Console.WriteLine("Баланс на счету: {0} рублей", checkWife.getBalance());
+            checkWife.PrintType(Check._Type.Wife);
+
+
+
+            Console.ReadKey(true);
         }
     }
 }
